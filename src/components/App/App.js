2a707b55af0dbeaf273c8377959f29e644c6e6d7
Unknown <lecke.martin@gmail.com>
2019-01-09 09:34:41 +0100

    Change structure for react router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
+import { withRouter } from "react-router";
 import {
   Collapse,
   Navbar,
@@ -8,10 +9,10 @@ import {
   Nav,
   NavItem,
   NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
+  // UncontrolledDropdown,
+  // DropdownToggle,
+  // DropdownMenu,
+  // DropdownItem,
   InputGroup,
   InputGroupAddon,
   Button,
@@ -21,7 +22,6 @@ import "./App.scss";
 
 const Home = () => <div>Home</div>;
 const Search = () => <div>Search</div>;
-const Troll = () => <div>Troll</div>;
 
 class App extends Component {
   state = {
@@ -36,58 +36,56 @@ class App extends Component {
 
   render() {
     return (
-      <Router>
-        <div className="App">
-          <header>
-            <Navbar color="light" light expand="md">
-              <NavbarBrand tag={Link} to="/">
-                Boardgame App
-              </NavbarBrand>
-              <NavbarToggler onClick={this.toggle} />
-              <Collapse isOpen={this.state.isOpen} navbar>
-                <Nav className="ml-auto" navbar>
-                  <NavItem>
-                    <InputGroup className="pt-4 pt-md-0">
-                      <Input />
-                      <InputGroupAddon addonType="append">
-                        <Button>Search</Button>
-                      </InputGroupAddon>
-                    </InputGroup>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink tag={Link} to="/troll">
-                      Home
-                    </NavLink>
-                  </NavItem>
-                  <NavItem>
-                    <NavLink tag={Link} to="/search">
-                      Search
-                    </NavLink>
-                  </NavItem>
-                  <UncontrolledDropdown nav inNavbar>
-                    <DropdownToggle nav caret>
-                      My Page
-                    </DropdownToggle>
-                    <DropdownMenu right>
-                      <DropdownItem>My games</DropdownItem>
-                      <DropdownItem divider />
-                      <DropdownItem>Log out</DropdownItem>
-                    </DropdownMenu>
-                  </UncontrolledDropdown>
-                </Nav>
-              </Collapse>
-            </Navbar>
-          </header>
-          <main>
-            <Route path="/" exact component={Home} />
-            <Route path="/search" component={Search} />
-            <Route path="/troll" component={Troll} />
-          </main>
-          <footer>footer</footer>
-        </div>
-      </Router>
+      <div className="App">
+        <header>
+          <Navbar color="light" light expand="md">
+            <NavbarBrand tag={Link} to="/">
+              Boardgame App
+            </NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <InputGroup className="pt-4 pt-md-0">
+                    <Input />
+                    <InputGroupAddon addonType="append">
+                      <Button>Search</Button>
+                    </InputGroupAddon>
+                  </InputGroup>
+                </NavItem>
+                <NavItem>
+                  <NavLink tag={Link} to="/">
+                    Home
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink tag={Link} to="/search">
+                    Search
+                  </NavLink>
+                </NavItem>
+                {/* User Account menu */}
+                {/* <UncontrolledDropdown nav inNavbar>
+                  <DropdownToggle nav caret>
+                    My Page
+                  </DropdownToggle>
+                  <DropdownMenu right>
+                    <DropdownItem>My games</DropdownItem>
+                    <DropdownItem divider />
+                    <DropdownItem>Log out</DropdownItem>
+                  </DropdownMenu>
+                </UncontrolledDropdown> */}
+              </Nav>
+            </Collapse>
+          </Navbar>
+        </header>
+        <main>
+          <Route path="/" exact component={Home} />
+          <Route path="/search" component={Search} />
+        </main>
+        <footer>footer</footer>
+      </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
