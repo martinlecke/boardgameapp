8805aecf9a8c6aaa1edab1f828d419a600d55c5f
Unknown <lecke.martin@gmail.com>
2019-01-12 10:48:31 +0100

    Add Express server

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static(path.join(__dirname, '../build')));
+
+app.get('/test', function (req, res) {
+  return res.send('test');
+});
+
+// app.get('/', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
+
+app.listen(PORT, () => {
+  console.log("Express is running on port", PORT);
+});
