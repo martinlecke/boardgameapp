dd4d516504a40a045a542f3b3bc4353356dafa97
Unknown <lecke.martin@gmail.com>
2019-01-13 18:39:56 +0100

    Install Mongoose

--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const bodyParser = require('body-parser')
-const path = require('path');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, '../build')));
+app.use(express.static(path.join(__dirname, "../build")));
 
 app.use(bodyParser.json());
 
-app.get('/test', function (req, res) {
-  return res.send('test');
+app.get("/test", function(req, res) {
+  return res.send("test");
 });
 
 // app.get('/', function (req, res) {
