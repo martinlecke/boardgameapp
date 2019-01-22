16d2d013768768810caef7471924d921f906d0f3
Unknown <lecke.martin@gmail.com>
2019-01-23 00:00:22 +0100

    Refactor Backend Api Routes

--- server/index.js
+++ server/index.js
@@ -5,13 +5,10 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const session = require("express-session");
 const User = require("./models/User");
-const Boardgame = require("./models/Boardgame");
 const PORT = process.env.PORT || 8080;
 const MongoStore = require("connect-mongo")(session);
 const cors = require("cors");
-const parseString = require("xml2js").parseString;
-const axios = require("axios");
-const _ = require("lodash");
+const apiRoutes = require('./routes/api');
 
 mongoose
   .connect(
@@ -132,115 +129,9 @@ app.post("/user/register", async (req, res) => {
   });
 });
 
-async function filterApiResponse(response, gameId) {
-  const allowed = [
-    "image",
-    "name",
-    "yearpublished",
-    "description",
-    "minplayers",
-    "maxplayers",
-    "playingtime",
-    "minplaytime",
-    "maxplaytime",
-    "statistics",
-    "averageweight",
-    "minage"
-  ];
-  const filtered = Object.keys(response)
-    .filter(key => allowed.includes(key))
-    .reduce((obj, key) => {
-      return {
-        ...obj,
-        [key]: response[key]
-      };
-    }, {});
 
-  const boardgame = await new Boardgame({
-    image: filtered.image,
-    yearPublished: filtered.yearpublished[0]["$"].value,
-    minPlayers: filtered.minplayers[0]["$"].value,
-    maxPlayers: filtered.maxplayers[0]["$"].value,
-    playingTime: filtered.playingtime[0]["$"].value,
-    title: filtered.name[0]["$"].value,
-    description: filtered.description[0],
-    bggRating: Number(
-      filtered.statistics[0].ratings[0].average[0]["$"].value
-    ).toFixed(2),
-    complexity: Number(
-      filtered.statistics[0].ratings[0].averageweight[0]["$"].value
-    ).toFixed(2),
-    age: filtered.minage[0]["$"].value,
-    gameId
-  });
-  await boardgame.save();
 
-  return boardgame;
-}
-
-app.get("/api/boardgame/:gameId", async (req, res) => {
-  const gameId = req.params.gameId;
-  Boardgame.findOne({ gameId }).then(game => {
-    if (game) {
-      res.json(game);
-    } else {
-      axios
-        .get(`https://www.boardgamegeek.com/xmlapi2/thing`, {
-          params: {
-            id: gameId,
-            videos: 1,
-            stats: 1
-          }
-        })
-        .then(response => {
-          parseString(response.data, async function(err, result) {
-            const filtered = await filterApiResponse(
-              result.items.item[0],
-              gameId
-            );
-
-            res.json(filtered);
-          });
-        })
-        .catch(e => {
-          res.send("error");
-        });
-    }
-  });
-});
-
-app.get("/api/search", (req, res) => {
-  console.log(req.query);
-  if (!_.isEmpty(req.query)) {
-    req.query.type = "boardgame";
-    req.query.exact = req.query.exact || 0;
-    axios
-      .get("https://www.boardgamegeek.com/xmlapi2/search", {
-        params: req.query
-      })
-      .then(async response => {
-        parseString(response.data, async (err, result) => {
-          const results = await result.items.item
-            .map(game => {
-              if (game["yearpublished"]) {
-                const gameId = game["$"].id,
-                  title = game["name"][0]["$"].value,
-                  yearPublished = game["yearpublished"][0]["$"].value;
-                return { gameId, title, yearPublished };
-              }
-              return false;
-            })
-            .filter(game => game);
-          res.send(results.splice(0,30));
-        });
-      })
-      .catch(e => {
-        console.log("error att hämta", e);
-      });
-  } else {
-    res.send("Det fanns ingen sökterm");
-  }
-});
+app.use('/api', apiRoutes)
 
 // /**
 //  * Filtering xml api call to JSON with keys defined in Allowed array
