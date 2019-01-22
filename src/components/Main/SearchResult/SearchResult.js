20f87127559794b74ab2c9d8be6e11a9ab79d237
Unknown <lecke.martin@gmail.com>
2019-01-22 18:10:01 +0100

    Render results from search api

--- src/components/Main/SearchResult/SearchResult.js
+++ src/components/Main/SearchResult/SearchResult.js
@@ -1,34 +1,35 @@
 import React, { Component } from "react";
 import { Col } from "reactstrap";
-import SingleResult from './SingleResult/SingleResult';
-import queryString from "query-string";
-import myResults from '../_temp' // Temp
-import axios from 'axios';
+import SingleResult from "./SingleResult/SingleResult";
+import axios from "axios";
 import "./SearchResult.scss";
 
 class SearchResult extends Component {
+  state = {
+    games: []
+  };
   componentDidMount() {
-    const values = this.props.location.search.substr(1);
-    console.log(values);
-    axios.get(`http://localhost:8080/api/search?${values}`)
-      .then(response => {
-        console.log(response.data)
-      });
+    const searchQuery = this.props.location.search;
+    axios
+      .get(`http://localhost:8080/api/search${searchQuery}`)
+      .then(response => {this.setState({games: response.data})});
   }
   render() {
     const header = {
-      title: 'Title',
-      year: 'Year',
-      bggRating: 'Rating'
-    }
-    return <Col className="main_box_ui py-3 px-4">
+      title: "Title",
+      year: "Year",
+      bggRating: "Rating"
+    };
+    return (
+      <Col className="main_box_ui py-3 px-4">
         <SingleResult header={header} />
         <div className="d-flex flex-column">
-          {myResults.map(game => (
-            <SingleResult key={game._id} game={game} />
+          {this.state.games && this.state.games.map(game => (
+            <SingleResult key={game.gameId} game={game} />
           ))}
         </div>
-      </Col>;
+      </Col>
+    );
   }
 }
 
--- src/components/Main/SearchResult/SingleResult/SingleResult.js
+++ src/components/Main/SearchResult/SingleResult/SingleResult.js
@@ -17,15 +17,15 @@ const singleResult = props => {
     );
   }
   return (
-    <Link to={`/boardgame/${props.game._id}`} payload={props.game} className="d-flex single-result">
+    <Link to={`/boardgame/${props.game.gameId}`} payload={props.game} className="d-flex single-result">
       <div>
-        <img src={ props.game.picture } alt={ props.game.title } />
+        <img src={ props.game.image && props.game.image } alt={ props.game.title } />
       </div>
       <h3 className="ml-4 align-self-center">
-        { props.game.title } <small>({ props.game.year })</small>
+        { props.game.title } <small>({ props.game.yearPublished })</small>
       </h3>
       <div className="ml-auto align-self-center bgg-rating">
-        { props.game.bggRating }
+        { props.game.bggRating && props.game.bggRating }
       </div>
     </Link>
   );
